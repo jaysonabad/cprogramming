59e430a1a3f67cbcf1983d690b8b95ce21428d73
Jayson Abad <jayson_abad@outlook.com>
2025-03-02 18:53:13 +0800

    ...

---
+++ README.md
@@ -0,0 +1,3 @@
+# C Practical Programming
+
+<p>There should be a note here.</p>
---
+++ sumoftwonumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+  // Declaration of integer variables.
+  int value1, value2, sum;
+  // Assigning values to the declared variable.
+  value1 = 50;
+  value2 = 25;
+  // Adding of two variable with declared values.
+  sum = value1 + value2;
+  // Printing the processed variables.
+  printf("The sum of %i and %i is %i\n", value1, value2, sum);
+
+  return 0;
+}
